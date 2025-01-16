b99ff2dfbc41bd96f0ab6b0e5f3d454c37f56910
Adam Chrimes <iChrimes@live.co.uk>
2025-01-16 15:35:48 +0000

    display basic NHS profile

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { auth } from "@/app/lib/auth";
 import SignInButton from "@/app/components/signInButton";
 import SignOutButton from "./components/signOutButton";
+import { formatNhsNumber, formatDate } from "@/app/lib/utils";
 
 export const metadata: Metadata = {
   title: `Overview - ${process.env.SERVICE_NAME}`,
@@ -13,10 +14,17 @@ export default async function Home() {
   return (
     <main className="nhsuk-main-wrapper" id="maincontent" role="main">
       <h1>Overview</h1>
+      <p>Public content.</p>
       {!session?.user && <SignInButton />}
       {session?.user && (
         <>
-          <p>You are signed in.</p>
+          <p>
+            Welcome to {session.user.name} (NHS number:{" "}
+            {formatNhsNumber(session.user.nhsNumber)}), you are signed in.
+          </p>
+          <p>Date of birth: {formatDate(session.user.dob)}</p>
+          <p>Identity level: {session.user.identityLevel}</p>
+          <p>Private content.</p>
           <SignOutButton />
         </>
       )}
