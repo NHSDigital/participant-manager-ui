b20c490c3a47ee19e0b07cf5f21c7a3f3541454c
Adam Chrimes <iChrimes@live.co.uk>
2025-01-27 11:48:23 +0000

    remove duplicate web folder

--- web/e2e/example.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from "@playwright/test";
-
-test("navigate to the Index page", async ({ page }) => {
-  await page.goto("http://localhost:3000/");
-  await expect(page).toHaveTitle(/Overview/);
-});
